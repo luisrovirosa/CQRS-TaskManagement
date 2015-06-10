426ecfed2f0392a80d06cc7346b356a43f340b38
Luis Rovirosa <luisrovirosa@gmail.com>
2015-06-10 20:08:14 +0200

    InMemoryTaskRepository testt refactoring

--- test/Task/InMemoryTaskRepositoryTest.php
+++ test/Task/InMemoryTaskRepositoryTest.php
@@ -3,27 +3,50 @@
 namespace CQRS\Test\Task;
 
 use CQRS\Task\InMemoryTaskRepository;
+use CQRS\Task\TaskRepository;
 
 class InMemoryTaskRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     const TASK_NAME = 'My task name';
 
+    /** @var TaskRepository */
+    protected $repository;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->repository = new InMemoryTaskRepository();
+        $this->repository->createTask(self::TASK_NAME);
+    }
+
     /** @test */
     public function it_retrieves_a_task_after_create_task()
     {
-        $repository = new InMemoryTaskRepository();
-        $repository->createTask(self::TASK_NAME);
-        $allTasks = $repository->findAll();
-        $this->assertCount(1, $allTasks);
+        $this->assertCount(1, $this->findAllTasks());
     }
 
     /** @test */
     public function the_task_has_the_given_name()
     {
-        $repository = new InMemoryTaskRepository();
-        $repository->createTask(self::TASK_NAME);
-        $allTasks = $repository->findAll();
-        $task = $allTasks[0];
-        $this->assertEquals(self::TASK_NAME, $task->name());
+        $this->assertEquals(self::TASK_NAME, $this->findLastTask()->name());
+    }
+
+    /**
+     * @return mixed
+     */
+    private function findAllTasks()
+    {
+        return $this->repository->findAll();
+    }
+
+    /**
+     * @return mixed
+     */
+    private function findLastTask()
+    {
+        $allTasks = $this->findAllTasks();
+        $lastTaskPosition = count($allTasks) - 1;
+
+        return $allTasks[$lastTaskPosition];
     }
 }
